566f27154032b33fcadf325631eb904d6be3e9a1
Moldytzu <m.alex123@yahoo.com>
2022-03-05 21:52:46 +0200

    reboot and shutdown

---
+++ src/reboot.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+#include <sys/reboot.h>
+
+int main()
+{
+    sync(); // sync filesystems
+    reboot(RB_AUTOBOOT); // do reboot
+    return 0;
+}
---
+++ src/shutdown.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+#include <sys/reboot.h>
+#include <linux/reboot.h>
+
+int main()
+{
+    sync(); // sync filesystems
+    reboot(LINUX_REBOOT_CMD_POWER_OFF); // do shutdown
+    return 0;
+}
