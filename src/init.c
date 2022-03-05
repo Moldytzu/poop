0b3efd1920b139be5c2cbd42f6397fb64f173d22
Moldytzu <m.alex123@yahoo.com>
2022-03-05 18:41:04 +0200

    executing our shell

--- src/init.c
+++ src/init.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "plib.h"
+
+void hang()
+{
+    printf("Hanging...\n"); // display a message
+    while(1); // while loop of death
+}
 
 int main()
 {
     // disable stdout buffering
     setvbuf(stdout, NULL, _IONBF, 0);
 
-    printf("poop init system\n");
+    printf("poop Init System\n");
 
     // execute our shell
-    printf("executing: /bin/psh\n");
-    execute("/bin/psh");
-
-    while(1);
+    printf("Executing: /bin/psh\n");
+    int status = execute("/bin/psh");
+    if(status != 0)
+    {
+        printf("Failed. Returned %d",status);
+    }
+    
+    hang();
     return 0;
 }
--- src/plib.h
+++ src/plib.h
@@ -1,7 +1,24 @@
 #include <unistd.h>
 #include <stdint.h>
+#include <signal.h>
+#include <stdio.h>
+#include <sys/wait.h>
 
-uint32_t execute(const char *path)
+int execute(const char *path)
 {
-    return execv(path, NULL);
+    int pid;
+    switch(pid = vfork()) // spawn child
+    {
+        case 0: // child
+        {
+            char *argv[] = {(char*)path, NULL};
+            execv(path,argv); // execute
+            break;
+        }
+        default: break; // do nothing
+    }
+
+    int status;
+    waitpid(pid, &status, 0); // wait for pid to quit 
+    return status;
 }
