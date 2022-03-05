593b8a3a7770a312356c020df7b7dd118fcfdac4
Moldytzu <m.alex123@yahoo.com>
2022-03-05 21:42:02 +0200

    arguments

--- src/psh.c
+++ src/psh.c
@@ -1,5 +1,31 @@
 #include "plib.h"
 
+char **args(char *cmd)
+{
+    int spaces = 0, len = strlen(cmd);
+
+    for (int i=0; i<len; i++) {
+        if (cmd[i] == ' ') { // delimiter
+            cmd[i] = 0; // split
+            spaces++;
+        }
+    }
+
+    char **argv = malloc(sizeof(char *) * ++spaces);
+    
+    int arg = 0;
+    argv[arg++] = cmd;
+
+    for (int i=0; i<len; i++) {
+        if (!cmd[i])
+            argv[arg++] = cmd + i + 1; // offset by i+1
+    }
+
+    argv[arg++] = 0; // terminate the array
+
+    return argv;
+}
+
 int main()
 {
     // disable stdout buffering
@@ -17,13 +43,14 @@ int main()
             exit(0); // exit
         else
         {
-            if(access(buffer,F_OK) != 0) // check if file exists
+            char **argv = args(buffer);
+            if(access(argv[0],F_OK) != 0) // check if file exists
             {
                 printf("Unknown command: %s\n", buffer);
                 continue;
             }
 
-            execute(buffer); // execute file
+            executea(argv[0],argv); // execute file
         }
     }
     return 0;
